1e66ee03c7fda01ce428b246d520f0bc3a4d9a28
Viraj <95890816+virajgsn@users.noreply.github.com>
2025-06-24 00:00:59 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,16 @@
+class employee:
+    name = "Akash"
+    salary = 50000
+    class phone:
+        mb_no = 9767902375
+        email = "sdedits1111@gmail.com"
+    
+
+
+obj1 = employee()                          # create object
+print("name of emplyee :  ",obj1.name)     #name of emplyee :   Akash
+print("salary of employee : ",obj1.salary) #salary of employee :  50000
+obj2 = obj1.phone()
+print("Phone number of employee is : ",obj2.mb_no)   #Phone number of employee is :  9767902375
+print("Email of employee is :",obj2.email)           #Email of employee is  sdedits1111@gmail.com
+               
